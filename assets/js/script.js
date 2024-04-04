e9ef915c154f4513b67bd50176b049913603cf5e
kristyyip <kristyyip51998@gmail.com>
2024-04-03 23:36:48 -0400

    fixed handleAddTask function and added code to the createTaskCard function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,43 @@
+// Grab references to the important DOM elements
+form = $("#form-modal")
+
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Todo: create a function to generate a unique task id
+// a function to generate a unique task id
 // src: https://dev.to/rahmanfadhil/how-to-generate-unique-id-in-javascript-1b13
 function generateTaskId() {
     let taskID = Math.floor(Math.random() * Date.now()).toString(16);
-    console.log(Date.now());
-    console.log(taskID);
     return taskID;
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const card = $("<div>")
+        .addClass("card task-card my-3")
+        .attr("data-task-id", task.id);
+    
+    const cardHeader = $("<div>")
+        .addClass("card-header h3")
+        .text(task.title);
+    
+    const cardBody = $("<div>").addClass("card-body");
+    const cardDescription = $("<p>")
+        .addClass("card-text")
+        .text(task.description)
+    const cardDueDate = $("<p>")
+        .addClass("card-text")
+        .text(task.dueDate);
+    
+    const cardDeleteBtn  = $("<button>")
+        .addClass("btn btn-danger delete")
+        .text("Delete")
+        .attr("data-task-id", task.id);
+    cardDeleteBtn.on("click", handleDeleteProject)
 
+    cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+    card.append(cardHeader, cardBody)
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -21,31 +45,50 @@ function renderTaskList() {
 
 }
 
-// Todo: create a function to handle adding a new task
-function handleAddTask(event){
-    $("#add-task").on("click", function () {
-        if (($("#title").val() === "") || ($("#due-date").val() === "") || ($("#description").val() === "")) {
-            $("#msg").text("Please make sure to fill out all fields to add a task.");
-        } else {
-            $("#msg").text("");
+// a function to handle adding a new task
+function handleAddTask(event) {
+    // prevent default behavior
+    event.preventDefault();
 
-            let taskID = generateTaskId();
-            let title = $("#title").val();
-            let dueDate = $("#due-date").val();
-            let description = $("#description").val();
+    // store user inputs into variables
+    const titleInput = $("#title").val();
+    const dueDateInput = $("#due-date").val();
+    const descriptionInput = $("#description").val();
 
-            let task = {
-                taskID = taskID,
-                title = title,
-                dueDate = dueDate,
-                description = description
-            };
-            
-            $("#formModal").modal("hide");
+    // validation to make sure all fields are filled out
+    if ((titleInput === "") || (dueDateInput === "") || (descriptionInput === "")) {
+        $("#msg").text("Please make sure to fill out all fields to add a task.");
+    } else {
+        $("#msg").text("");
 
-            return task;
+        // create object of user inputs for task
+        const newTask = {
+            id: generateTaskId(),
+            title: titleInput,
+            dueDate: dueDateInput,
+            description: descriptionInput,
+            status: "to-do"
+        };
+         
+        // If no tasks were retrieved from localStorage, assign tasks to a new empty array to push to later
+        if (!taskList) {
+            taskList = [];
         }
-    })
+
+        // add new task to task list array
+        taskList.push(newTask);
+
+        // create key:value pair of task list array in local storage
+        localStorage.setItem("tasks", JSON.stringify(taskList));
+
+        // clear form input
+        $("#title").val("");
+        $("#due-date").val("");
+        $("#description").val("");
+        
+        // exit out of modal
+        form.modal("hide");
+    }
 }
 
 // Todo: create a function to handle deleting a task
@@ -61,12 +104,15 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
     // open modal when user clicks on the button "Add Task"
-    $("#formModal").on("click", function (event) {
-        $("#formModal").modal("show");
+    form.on("click", function (event) {
+        form.modal("show");
     });
 
     // close modal if user clicks on the X
     $(".close").on("click", function () {
-        $("#formModal").modal("hide");
+        form.modal("hide");
     })
+
+    // add user inputs into taskList array
+    $("#add-task").on("click", handleAddTask)
 });
